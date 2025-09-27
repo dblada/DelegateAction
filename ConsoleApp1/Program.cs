422d066754f5cbe37b391638a35e561d0b67898f
Dariusz <dariusz.blada@gmail.com>
2025-09-27 12:39:03 +0200

    Added logic of program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var names = new List<string> {"Darek", "Marzena", "Justyna"};
+
+            Action<string> greet = name => Console.WriteLine($"Hello, {name}!");
+
+            names.ForEach(greet);
+
+            // Możesz przypisać istniejącą metodę do Action
+            Action<string> logger = ShowMessage;
+
+            logger("Aplikacja uruchomiona");
+
+            // Action jako parametr metody
+            ProcessUsers(new List<string> { "Darek", "Adam", "Łukasz" }, name => Console.WriteLine($"Witaj, {name}!"));
+
+            Console.ReadKey();
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine($"[INFO] {message}");
+        }
+
+        static void ProcessUsers(List<string> users, Action<string> action)
+        {
+            foreach (var user in users) {
+                action(user);
+            }
         }
     }
 }
